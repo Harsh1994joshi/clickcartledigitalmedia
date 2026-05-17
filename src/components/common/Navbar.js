7b0633c661ecbd6d9593aa9bbb9d32998aa25004
pashankargasagency <pashankargas.web@gmail.com>
2026-05-17 10:49:15 +0530

    Initial commit

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
-import logo from '../../assets/logo.jpg';
+import logo from '../../assets/logo.png';
 
 <img src={logo} alt="Logo" />
 
